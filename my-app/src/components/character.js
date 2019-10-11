fe211378c33859d14aa05c949a51960e980e2960
mlomelisa <monica.lomelim@gmail.com>
2019-10-11 11:05:19 -0700

    Able to detect more than once image select

--- my-app/src/components/character.js
+++ my-app/src/components/character.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import characters from '../characters.json';
-import {Col,Row,Container} from './Grid';
+import {Container} from './Grid';
 import Header from './Header';
 import Jumbotron from './jumbotron';
 import Thumbnail from './thumbnail';
@@ -9,8 +9,10 @@ import Thumbnail from './thumbnail';
 class CharactersContainer extends Component {
 state = {
   characters,
+  selected: [],
   count: 0,
-  topcount: 0
+  topcount: 0,
+  ischecked: 0
 }
 
 functionSort = (characters) => {
@@ -21,31 +23,52 @@ functionSort = (characters) => {
 }
 
 
-handleChange = event => {
+findId = (selected, userid) => {
+ 
+  function isSelected(item) {
+    return item === userid;
+  }
+  
+  if (selected.find(isSelected)) {
+    alert('Game Over')
+    
+  } else {
+    selected = [ ...selected, userid ]
+    console.log(selected)
+   return selected  
+  }
+
+}
+  
+
+
+handleChange = (event) => {
   event.preventDefault();
+
   
     this.setState({
+     // selected: [ ...this.state.selected, event.currentTarget.dataset.id ],
+      selected: this.findId(this.state.selected, event.currentTarget.dataset.id),
       count: this.state.count + 1,
-      checked: "true",
+      ischecked: event.currentTarget.dataset.id,
       characters: this.functionSort(this.state.characters)
      })      
-  
+    
 }
 
-
-
 render() {
-  console.log(this.state.count)
+  
   return (
     <div>
    <Header count={this.state.count} topcount={this.state.topcount} />
     <Jumbotron />
     <Container>
-      {this.state.characters.map(character => {        
-        return(
-          
-          <div key={character.id} onClick={this.handleChange} >
-          <Thumbnail src={character.img} ischecked={character.checked}/>
+      {this.state.characters.map(character => {     
+       
+        return(       
+            
+          <div  key={character.id} onClick={this.handleChange} data-id={character.id}  >
+          <Thumbnail src={character.img} id={character.id} ischecked={this.state.ischecked == character.id ? '1' : '0' } />
         </div>
         )
       }  
--- my-app/src/components/thumbnail/index.js
+++ my-app/src/components/thumbnail/index.js
@@ -5,13 +5,16 @@ import "./style.css";
 // It will always keep square proportions at any size without the image warping
 // The "role" and "aria label" are there to identify the element's purpose as an image for accessibility purposes
 function Thumbnail(props) {
- console.log(props)
+
   return (
     <div 
       className="thumbnail click-item shake"
       ischecked={props.ischecked}
+      status={props.value}
+      id={props.id}
       role="img"
       aria-label="click item"
+      onClick={props.handleChange}
       style={{
         backgroundImage: `url(${props.src})`
       }}
