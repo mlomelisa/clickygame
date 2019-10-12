2dbcde58361ac9abb432782be5f2ffbc067ad170
mlomelisa <monica.lomelim@gmail.com>
2019-10-11 23:38:34 -0700

    Updates

---
+++ precache-manifest.7bac4b721c5bcb8a5bf9c7fa80b0d10f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "901125318b6d5e618eb4be643a226e73",
+    "url": "/clickygame/index.html"
+  },
+  {
+    "revision": "8eccd5ac7c1c76b1f29e",
+    "url": "/clickygame/static/css/main.0eae0a84.chunk.css"
+  },
+  {
+    "revision": "df40e83126ba554131f1",
+    "url": "/clickygame/static/js/2.ac633d95.chunk.js"
+  },
+  {
+    "revision": "8eccd5ac7c1c76b1f29e",
+    "url": "/clickygame/static/js/main.790b0a8e.chunk.js"
+  },
+  {
+    "revision": "c1ab281b622b480ee35e",
+    "url": "/clickygame/static/js/runtime-main.84b2e726.js"
+  },
+  {
+    "revision": "87ee159d06856983c40e4ead927ff7f6",
+    "url": "/clickygame/static/media/wallpaper.87ee159d.jpg"
+  }
+]);
